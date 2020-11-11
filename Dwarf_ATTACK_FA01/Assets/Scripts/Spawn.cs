b1a3db7897e6b35ca171a1dcff5937de56dd4d48
SEOKDONGYUN <65929190+SEOKDONGYUN@users.noreply.github.com>
2020-11-11 13:17:29 +0900

    aaa

---
+++ Dwarf_ATTACK_FA01/Assets/Scripts/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject prefab;
+    //Transform taget;
+
+    public float spawnRate = 1f;
+    float time;
+
+    float tMax = 3f;
+    float tMin = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (!GameManager.instance.isGameover)
+        {
+            if (time > spawnRate)
+            {
+                time = 0;
+                spawnRate = Random.Range(tMin, tMax);
+
+                GameObject b = Instantiate(prefab, transform.position, transform.rotation);
+                //b.transform.LookAt(taget);
+            }
+        }
+    }
+}
---
+++ Dwarf_ATTACK_FA01/Assets/Scripts/UIButtonManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIButtonManger : MonoBehaviour
+{
+    GameObject player;
+    PlayerController playerScript;
+
+    public void Init()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+    }
+
+    public void JumpClick()
+    {
+        Debug.Log("jump");
+        playerScript.inputjump();
+    }
+
+    public void AtteckClick()
+    {
+        Debug.Log("Atteck");
+    }
+}
