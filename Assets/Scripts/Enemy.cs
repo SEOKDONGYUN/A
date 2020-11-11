f384203d884a91e0f0f8b85504bcf2a190bf61b5
SEOKDONGYUN <65929190+SEOKDONGYUN@users.noreply.github.com>
2020-11-11 16:30:28 +0900

    update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public AudioClip deathClip;
+    public Animator animator;
+
+    public int maxHealth = 100;
+
+    int currentHealth;
+
+    private AudioSource playerAudio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        playerAudio = GetComponent<AudioSource>();
+                
+    }
+
+    public void TakeDamege(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        animator.SetTrigger("Die");
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        GameManager.instance.AddScore(1);
+
+        Destroy(gameObject, 10f);
+
+        
+    }
+}
